cd07a292d787609e9df7def7c55da2501b808076
Craig Hellinger <craig.hellinger@jadu.co.uk>
2021-02-20 22:51:32 +0000

    Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap' }
     ]
